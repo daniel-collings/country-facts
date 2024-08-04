a41d47d924630bbf7c8105b7f15089057c0f2ea0
Daniel Collings <collings8915@gmail.com>
2024-08-04 11:35:27 +0100

    Add .env.development and update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_ENV === 'dev' ? '/' : '/country-facts/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets'
+    },
+    server: {
+      port: 3000
+    },
+    preview: {
+      port: 4173
     }
-  },
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
-  server: {
-    port: 3000
-  },
-  preview: {
-    port: 4173
   }
 })
