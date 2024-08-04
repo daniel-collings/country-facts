7431e56737759dbed8e81ba9160d29d80fffbd9f
Daniel Collings <collings8915@gmail.com>
2024-08-04 11:38:09 +0100

    Update router to use hashRouter for github pages

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,6 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import countries from '@/routes/countries.tsx'
 
-const router = createBrowserRouter([...countries])
+const router = createHashRouter([...countries])
 
 export default router
