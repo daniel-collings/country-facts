5e3a43d6bbaf1af3f195dfe7ab00288ac7ef34fd
Daniel Collings <collings8915@gmail.com>
2024-08-06 21:11:21 +0100

    Add Countries.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,8 +2,10 @@ import { ReactNode } from 'react'
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <div data-theme="light" className="h-screen">
-      <main className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
+    <div data-theme="light" className="h-full bg-base-content p-8">
+      <main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 bg-base-200 rounded-xl p-8">
+        {children}
+      </main>
     </div>
   )
 }
--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -1,3 +1,72 @@
+import countriesData from '@/data/countries.json'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 export default function Countries() {
-  return <>Countries</>
+  const [searchCountryList, setSearchCountryList] = useState<string>()
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="input input-bordered"
+        onChange={(e) => {
+          setSearchCountryList(e.target.value)
+        }}
+      />
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <td>Country</td>
+            <td>Flag</td>
+            <td>Continent</td>
+          </tr>
+        </thead>
+        <tbody>
+          {countriesData
+            .filter(
+              (f) =>
+                f.name.common
+                  .toLowerCase()
+                  .includes(searchCountryList?.toLowerCase()) ||
+                !searchCountryList ||
+                f.flag === searchCountryList ||
+                f.continents.some((continent) =>
+                  continent
+                    .toLowerCase()
+                    .includes(searchCountryList?.toLowerCase())
+                )
+            )
+            .map((country, i) => (
+              <tr key={i}>
+                <td>
+                  <Link
+                    to={`/countries/${encodeURIComponent(country.name.common)}`}
+                  >
+                    {country.name.common}{' '}
+                  </Link>
+                </td>
+                <td>
+                  <Link
+                    to={`/countries/${encodeURIComponent(country.name.common)}`}
+                  >
+                    {country.flag}{' '}
+                  </Link>
+                </td>
+                <td>
+                  <Link
+                    to={`/countries/${encodeURIComponent(country.name.common)}`}
+                  >
+                    {country.continents.map((continent, i) => (
+                      <td key={`${country.area}_${i}`}>{continent}</td>
+                    ))}
+                  </Link>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
