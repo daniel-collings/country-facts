8b94dbd721436e0859b0cf43711bcfbced976998
Daniel Collings <20369882+daniel-collings@users.noreply.github.com>
2024-08-06 21:44:14 +0100

    Update countries page and styling (#8)
    * Add Countries.tsx
    
    * Fix issue with child elements in Countries.tsx table

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,8 +2,10 @@ import { ReactNode } from 'react'
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <div data-theme="light" className="h-screen">
-      <main className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
+    <div data-theme="light" className="h-screen bg-base-content p-4 sm:p-8">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-base-200 rounded-xl p-8">
+        {children}
+      </main>
     </div>
   )
 }
--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -1,3 +1,71 @@
+import countriesData from '@/data/countries.json'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 export default function Countries() {
-  return <>Countries</>
+  const [searchCountryList, setSearchCountryList] = useState<string>()
+
+  return (
+    <div className="space-y-4">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="input input-bordered"
+        onChange={(e) => {
+          setSearchCountryList(e.target.value)
+        }}
+      />
+      <div className="block h-96 overflow-auto overflow-x-auto">
+        <table className="table table-sm table-pin-rows">
+          <thead>
+            <tr>
+              <td>Country</td>
+              <td>Flag</td>
+              <td>Continent</td>
+            </tr>
+          </thead>
+          <tbody>
+            {countriesData
+              .filter(
+                (f) =>
+                  f.name.common
+                    .toLowerCase()
+                    .includes(searchCountryList?.toLowerCase()) ||
+                  !searchCountryList ||
+                  f.flag === searchCountryList ||
+                  f.continents.some((continent) =>
+                    continent
+                      .toLowerCase()
+                      .includes(searchCountryList?.toLowerCase())
+                  )
+              )
+              .map((country, i) => (
+                <tr key={i} className="hover">
+                  <td>
+                    <Link
+                      to={`/countries/${encodeURIComponent(country.name.common)}`}
+                    >
+                      {country.name.common}{' '}
+                    </Link>
+                  </td>
+                  <td className="text-center sm:text-left">
+                    <Link
+                      to={`/countries/${encodeURIComponent(country.name.common)}`}
+                    >
+                      {country.flag}{' '}
+                    </Link>
+                  </td>
+                  <td className="text-wrap">
+                    {country.continents.reduce(
+                      (p, c, i) => p + (i > 0 ? `, ${c}` : c),
+                      ''
+                    )}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
