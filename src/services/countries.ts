25e92e14e6d72e369570bf4af57bd7bde2eb9425
Daniel Collings <collings8915@gmail.com>
2024-08-07 08:16:00 +0100

    Improve type of fetchCountries fn

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,4 +1,6 @@
-export const fetchCountries = async (inputValue: string): Promise<void> =>
+export const fetchCountries = async (
+  inputValue: string
+): Promise<Record<string, unknown>[]> =>
   await fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then((res) =>
     res.json()
   )
