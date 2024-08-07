06657791572f1af377957e4919214a72f5be5685
Daniel Collings <collings8915@gmail.com>
2024-08-07 21:21:59 +0100

    Add tanstack query dependency

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { RouterProvider } from 'react-router-dom'
 import React from 'react'
 import MainLayout from '@/layouts/MainLayout.tsx'
 import router from '@/routes/router.ts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <MainLayout>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </MainLayout>
   )
 }
