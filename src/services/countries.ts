cbf04ddfc45147ff5061dfc62cbe551cac3b8f2a
Daniel Collings <collings8915@gmail.com>
2024-08-15 16:06:16 +0100

    Add fetchContryByCode service

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -4,3 +4,10 @@ export const fetchCountries = async (
   await fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then((res) =>
     res.json()
   )
+
+export const fetchCountryByCode = async (
+  inputValue: string
+): Promise<Record<string, unknown>[]> =>
+  await fetch(
+    `https://restcountries.com/v3.1/alpha/${inputValue}?fields=name`
+  ).then((res) => res.json())
