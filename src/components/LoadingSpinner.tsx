499e152b3aabd711f3627d7e3dac0abbac124224
Daniel Collings <collings8915@gmail.com>
2024-08-15 20:07:22 +0100

    Add LoadingSpinner.tsx

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="flex w-full items-center h-96">
+      <div className="loading loading-spinner loading-lg text-primary mx-auto h-full" />
+    </div>
+  )
+}
