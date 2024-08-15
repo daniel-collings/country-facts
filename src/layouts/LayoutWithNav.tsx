562115a8cb3d4d1b30efc864c59f23e0c8d42427
Daniel Collings <collings8915@gmail.com>
2024-08-15 20:08:03 +0100

    Enable user to switch theme via navbar

--- src/layouts/LayoutWithNav.tsx
+++ src/layouts/LayoutWithNav.tsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useTheme } from '@/hooks/useTheme.tsx'
+import { ThemeEnum } from '@/types/enums.ts'
 
 export default function LayoutWithNav({
   children
 }: {
   children: React.ReactNode
 }) {
+  const { setTheme } = useTheme()
+
   return (
     <div className="space-y-4">
       <div className="navbar bg-base-200 rounded-xl">
@@ -16,7 +20,18 @@ export default function LayoutWithNav({
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <Link to="/">Home</Link>
+              <details>
+                <summary>Theme</summary>
+                <ul className="bg-base-100 rounded-t-none p-2 max-h-56 overflow-auto">
+                  {Object.keys(ThemeEnum).map((theme) => (
+                    <li key={theme}>
+                      <button onClick={() => setTheme(ThemeEnum[theme])}>
+                        {ThemeEnum[theme]}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </details>
             </li>
             <li>
               <Link to="/countries">Countries</Link>
