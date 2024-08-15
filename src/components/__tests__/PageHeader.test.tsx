449a9444e0eee8f2d57e5b45a8f2de43f193fc19
Daniel Collings <collings8915@gmail.com>
2024-08-15 11:05:17 +0100

    Update PageHeader tests

--- src/components/__tests__/PageHeader.test.tsx
+++ src/components/__tests__/PageHeader.test.tsx
@@ -3,8 +3,20 @@ import { render } from '@/utils/testUtils.tsx'
 import PageHeader from '@/components/PageHeader.tsx'
 
 describe('PageHeader Component', () => {
-  test('should render correctly', () => {
-    const { asFragment } = render(<PageHeader title="Hello World" />)
+  test('should render component correctly', () => {
+    const { asFragment, getByText } = render(<PageHeader title="Hello World" />)
     expect(asFragment()).toMatchSnapshot()
+    expect(getByText('Hello World')).toBeInTheDocument()
+  })
+
+  test('should render subtitle', () => {
+    const { getByText, getByTestId } = render(
+      <PageHeader title="Hello World" subtitle="Welcome to Country Facts" />
+    )
+
+    const subtitle = getByTestId('pageHeaderSubtitle')
+
+    expect(subtitle).toBeInTheDocument()
+    expect(getByText('Welcome to Country Facts')).toBeVisible()
   })
 })
