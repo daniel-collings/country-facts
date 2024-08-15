16ad51420e1cb41410da7b7ab7430e4a6624830a
Daniel Collings <collings8915@gmail.com>
2024-08-15 20:54:44 +0100

    Throw error if statusCode is >= 400

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,8 +1,11 @@
 export const fetchCountries = async (
   inputValue: string
 ): Promise<Record<string, unknown>[]> =>
-  await fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then((res) =>
-    res.json()
+  await fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then(
+    (res) => {
+      if (res.status >= 400) throw new Error('Something went wrong')
+      return res.json()
+    }
   )
 
 export const fetchCountryByCode = async (
@@ -10,4 +13,7 @@ export const fetchCountryByCode = async (
 ): Promise<Record<string, unknown>[]> =>
   await fetch(
     `https://restcountries.com/v3.1/alpha/${inputValue}?fields=name`
-  ).then((res) => res.json())
+  ).then((res) => {
+    if (res.status >= 400) throw new Error('Something went wrong')
+    return res.json()
+  })
