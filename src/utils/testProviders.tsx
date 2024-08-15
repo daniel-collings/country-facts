afc4f33fd12f12d37a3d1f75146707e76c510278
Daniel Collings <collings8915@gmail.com>
2024-08-15 20:06:59 +0100

    Add missing ThemeProvider from testProviders.tsx

--- src/utils/testProviders.tsx
+++ src/utils/testProviders.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import MainLayout from '@/layouts/MainLayout.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@/context/providers/ThemeProvider.tsx'
 
 const queryClient = new QueryClient()
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
-    <MainLayout>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>{children}</BrowserRouter>
-      </QueryClientProvider>
-    </MainLayout>
+    <ThemeProvider>
+      <MainLayout>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>{children}</BrowserRouter>
+        </QueryClientProvider>
+      </MainLayout>
+    </ThemeProvider>
   )
 }
 
