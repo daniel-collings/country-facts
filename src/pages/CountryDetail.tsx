f9bfc93e24c6bb5837eb084094d75d412c5664f6
Daniel Collings <collings8915@gmail.com>
2024-08-15 21:24:51 +0100

    Extract MultiMatches.tsx into own component

--- src/pages/CountryDetail.tsx
+++ src/pages/CountryDetail.tsx
@@ -11,6 +11,7 @@ import BasicDataDisplay from '@/components/BasicDataDisplay.tsx'
 import ImageCard from '@/components/ImageCard.tsx'
 import ErrorDisplay from '@/components/ErrorDisplay.tsx'
 import LoadingSpinner from '@/components/LoadingSpinner.tsx'
+import MultiMatches from '@/pages/MultiMatches.tsx'
 
 const excludeDetailsList = [
   'name',
@@ -39,32 +40,15 @@ export default function CountryDetail() {
   if (isLoading) return <LoadingSpinner />
   else if (isError) return <ErrorDisplay error={error} />
 
+  const countryData = data?.length
+    ? data.find((country) => country.name.common === countryId)
+    : []
   if (
     Array.isArray(data) &&
     data.length > 1 &&
-    countryId !== data[0].name.common
+    countryData?.name.common !== countryId
   ) {
-    return (
-      <div>
-        <CardFrame>
-          <div className="space-y-4">
-            <PageHeader title="Multiple Matches" />
-            <p>Did you mean?</p>
-            <ul>
-              {data.map((country, index) => (
-                <li key={index} className="hover:underline text-primary">
-                  <a
-                    href={`#/countries/${encodeURIComponent(country.name.common)}`}
-                  >
-                    {country.name.common}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </CardFrame>
-      </div>
-    )
+    return <MultiMatches data={data} />
   } else if (!data || !countryId) {
     return <div>Data is undefined</div>
   }
@@ -73,7 +57,7 @@ export default function CountryDetail() {
     <div className="space-y-4">
       <PageHeader
         title={countryId}
-        subtitle={data[0].altSpellings.reduce(
+        subtitle={countryData?.altSpellings.reduce(
           (p, c, i) => p + (i > 0 ? `, ${c}` : c),
           ''
         )}
@@ -84,7 +68,7 @@ export default function CountryDetail() {
           <label className="w-full grow">
             Select what information you want to see about {countryId}
           </label>
-          {Object.keys(data[0])
+          {Object.keys(countryData)
             .filter((f) => !excludeDetailsList.includes(f))
             .map((field) => (
               <label
@@ -109,32 +93,36 @@ export default function CountryDetail() {
 
       <CardFrame>
         <div className="flex flex-wrap items-center justify-evenly gap-4">
-          <ImageCard
-            png={data[0].flags.png}
-            alt={`${data[0].name.common} flag`}
-          />
+          {countryData?.flags?.png && (
+            <ImageCard
+              png={countryData?.flags?.png}
+              alt={`${data[0].name.common} flag`}
+            />
+          )}
           <div className="space-y-4">
             <BasicDataDisplay
-              data={data[0].capital as string | string[]}
+              data={countryData?.capital as string | string[]}
               label="Capital"
             />
-            {data[0].population && (
+            {countryData?.population && (
               <PopulationCapacityFact
-                population={data[0].population as number}
+                population={countryData.population as number}
               />
             )}
-            {data[0].area && <CountryAreaFact area={data[0].area as number} />}
+            {countryData?.area && (
+              <CountryAreaFact area={countryData.area as number} />
+            )}
           </div>
-          {data[0]?.coatOfArms?.png && (
+          {countryData?.coatOfArms?.png && (
             <ImageCard
-              png={data[0].coatOfArms.png}
-              alt={`${data[0].name.common} coat of arms`}
+              png={countryData.coatOfArms.png}
+              alt={`${countryData.name.common} coat of arms`}
             />
           )}
         </div>
       </CardFrame>
 
-      <DynamicDataView data={data[0]} viewableFields={viewableFields} />
+      <DynamicDataView data={[countryData]} viewableFields={viewableFields} />
     </div>
   )
 }
