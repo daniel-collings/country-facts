e03bac0b3ee52a08916d2d42abd5251640dc6c98
Daniel Collings <collings8915@gmail.com>
2024-08-17 15:49:33 +0100

    Update services to use api instance

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,19 +1,17 @@
+import api from '@/services/api.ts'
+
 export const fetchCountries = async (
   inputValue: string
 ): Promise<Record<string, unknown>[]> =>
-  await fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then(
-    (res) => {
-      if (res.status >= 400) throw new Error('Something went wrong')
-      return res.json()
-    }
-  )
+  await api(`/v3.1/name/${inputValue}`).then((res) => {
+    if (res.status >= 400) throw new Error('Something went wrong')
+    return res.data
+  })
 
 export const fetchCountryByCode = async (
   inputValue: string
 ): Promise<Record<string, unknown>[]> =>
-  await fetch(
-    `https://restcountries.com/v3.1/alpha/${inputValue}?fields=name`
-  ).then((res) => {
+  await api(`/v3.1/alpha/${inputValue}?fields=name`).then((res) => {
     if (res.status >= 400) throw new Error('Something went wrong')
-    return res.json()
+    return res.data
   })
